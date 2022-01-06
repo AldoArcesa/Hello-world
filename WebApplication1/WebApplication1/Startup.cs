3008b33649173691faef4f26a316b1bca59abce6
Alvaro Arce <alvaro.arce@dxc.com>
2022-01-06 13:32:46 -0600

    cuarto commit

--- WebApplication1/WebApplication1/Startup.cs
+++ WebApplication1/WebApplication1/Startup.cs
@@ -26,7 +26,7 @@ namespace WebApplication1
             services.AddRazorPages();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // 0000 This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
